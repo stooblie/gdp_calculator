62ccb9dda5860cc621e4fee0f8b1da2832e04cd5
Stephen Strnad <stooblie@gmail.com>
2019-01-06 16:32:10 -0600

    Put some intial framework in for handling web request ouput

--- src/output.py
+++ src/output.py
@@ -1,1 +1,12 @@
+import json
 
+class Output():
+    ''''''
+    def __init__(self, output):
+        self.output = None
+
+    def parse(self):
+        pass
+
+    def display(self):
+        pass
