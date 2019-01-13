280df1bffff13cf84fb79b424c207fad44b0271c
Stephen Strnad <stooblie@gmail.com>
2019-01-13 15:55:06 -0600

    Added some new files and continued collect.py development

--- etl/collect.py
+++ etl/collect.py
@@ -1,8 +1,13 @@
 import json
 from collections import defaultdict
 from api import Request
+import yaml
 
-#
+#with open("./src_bea/generic.table.yaml", 'r') as stream:
+    #try:
+        #print(yaml.load(stream)['columns'])
+    #except yaml.YAMLError as exc:
+        #print(exc)
 
 class Output():
     '''
@@ -10,17 +15,21 @@ class Output():
     '''
     def __init__(self, type, data):
         if type == 'bea':
-            self.handle = 'BEAAPI'
-        else self.handle = None
+            self.handle = "BEAAPI"
+        else: self.handle = None
         self.parameters = data[self.handle]['Request']['RequestParam']
-        self.results = data[self.type]['Request']['Results']
+        self.results = data[self.handle]['Results']['Data']
 
         self.table = None
         self.time = None
         self.frequency = None
 
+        # Keeping this here for future use. Define fields to look for depending on table.
+        #self.table_schema = None
+
     def parse(self):
         for entry in self.parameters:
+            column = lower(entry['ParameterName'])
             if entry['ParameterName'] == 'TABLENAME':
                 self.table = self.type + '_' + lower(entry['ParameterValue'])
             if entry['ParameterName'] == 'FREQUENCY':
@@ -32,10 +41,13 @@ class Output():
         for entry in data:
             column = lower(entry['LineDescription'].replace(' ', '_'))
             dict[column].append(entry['DataValue'])
-        pass
 
     def display(self):
         pass
 
 if __name__ == '__main__':
-    pass
+    # Example response web API.
+    data = json.load(open("./example_data.json", 'r'))
+
+    obj = Output('bea', data)
+    print(obj.results[0])
