3681ff1be863d669a2a2264ff2a48cda40049e0c
Stephen Strnad <stooblie@gmail.com>
2019-02-10 09:56:59 -0600

    Renaming file to qdl_request to avoid error.

--- etl/qdl_request.py
+++ etl/qdl_request.py
@@ -14,24 +14,26 @@ quandl.ApiConfig.api_key = os.environ['QUANDL_KEY']
 class Request():
     '''Handles making an API request through Quandl.'''
     def __init__(self, type='data', datatable_code=None, parameters=None, format='json'):
-        self.data_uri = quandl_dict['base_uri']['data']
-        self.metadata_uri = quandl_dict['base_uri']['metadata']
+        self.uri = quandl_dict['base_uri'][type]
         self.datatable_code = datatable_code
         self.format = format
         self.type = type
+        self.parameters = parameters
 
-    def get_data(self, **kwargs):
-        quandl.get(self.datatable_code, )
+    def get_data(self):
+        '''Make a data request'''
+        quandl.get(self.datatable_code, **self.parameters)
         pass
 
     def get_metadata(self):
+        '''Make a metadata request'''
         pass
 
 if __name__ == '__main__':
     code = 'FRED/GDP'
     params = {start_date: "2001-12-31", end_date: "2005-12-31"}
 
-    test = Request(datatable_code='FRED/')
+    test = Request(datatable_code=code, parameters=params)
     print(test)
 
     data = test.get_data()
